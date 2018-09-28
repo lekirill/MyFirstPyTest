e3203eeadce6f7070656e2e8ff1015375f912a62
Kirill Lesnikov <klesnikov@MacBook-Pro-Kirill.local>
2018-09-28 22:33:22 +0300

    third test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-a = input("What's your name? "  )
-print("Hello %s!" % (a))
-print ("v2")
-
+import openpyxl as xl_tool
+wb = xl_tool.load_workbook('./Слова_англ.xlsx')
+print(wb.sheetnames)
+ws = wb['Лист1']
+print(ws['A3'].value)
+wb.close()
