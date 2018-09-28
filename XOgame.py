a4eb5be635d2ff47c6b0de6ad5714f8ab5ad7d53
Kirill Lesnikov <klesnikov@MacBook-Pro-Kirill.local>
2018-09-29 00:20:32 +0300

    XO updated 2

--- XOgame.py
+++ XOgame.py
@@ -35,6 +35,7 @@ def winCheck():
        field[0][0] == field[1][0] == field[2][0] or \
        field[0][1] == field[1][1] == field[2][1] or \
        field[0][2] == field[1][2] == field[2][2] or \
-       field[0][1] == field[1][1] == field[0][2] or \
+       field[0][2] == field[1][1] == field[2][0] or \
        field[2][0] == field[2][1] == field[2][2] or \
-       field[0][1] == field[1][1] == field[1][2] or \
+       field[1][0] == field[1][1] == field[1][2]:
+        gaming = False
